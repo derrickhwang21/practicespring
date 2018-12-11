0f453417cfeb473fa5183ca567e69be4c3ae0757
dhwang21 <hwang.derrick@gmail.com>
2018-12-11 10:32:28 -0800

    finished controller

---
+++ src/main/java/com/hwang/practicespring/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.hwang.practicespring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+}
