1949b3737981e95fae934954f097e59baf66e93c
dhwang21 <hwang.derrick@gmail.com>
2018-12-11 09:36:16 -0800

    initial commit from Spring init

---
+++ src/main/java/com/hwang/practicespring/PracticespringApplication.java
@@ -0,0 +1,12 @@
+package com.hwang.practicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticespringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hwang/practicespring/PracticespringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hwang.practicespring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracticespringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
