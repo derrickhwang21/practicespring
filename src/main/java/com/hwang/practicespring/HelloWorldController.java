2b3e5047da1ab1ea0a72b055d23ef0164e410d91
dhwang21 <hwang.derrick@gmail.com>
2018-12-12 13:09:08 -0800

    initial commit

--- src/main/java/com/hwang/practicespring/HelloWorldController.java
+++ src/main/java/com/hwang/practicespring/HelloWorldController.java
@@ -1,6 +1,5 @@
 package com.hwang.practicespring;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
