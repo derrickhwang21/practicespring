50efe02383968040ed976daccc1afb92c6fdc591
dhwang21 <hwang.derrick@gmail.com>
2018-12-18 21:31:09 -0800

    finished integration test

--- src/main/java/com/hwang/practicespring/PracticespringApplication.java
+++ src/main/java/com/hwang/practicespring/PracticespringApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+
 public class PracticespringApplication {
 
 	public static void main(String[] args) {
